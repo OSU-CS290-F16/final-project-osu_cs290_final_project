7fc9c46bddfe81b130df3d66110984842693bd85
Gregory Vandelune <vandelug@onid.oregonstate.edu>
2016-12-09 11:11:35 -0800

    character drop down populated

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 //var path = require('path');
 var express = require('express');
 var exphbs = require('express3-handlebars');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 // Use Handlebars as the view engine for the app.
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
 
 var port = process.env.PORT || 3000;
 
@@ -24,9 +27,29 @@ app.get('/about', function(req, res) {
 });
 
 app.get('/create', function(req, res) {
-	res.render('create', {
-		title: 'Create Marathon'
+	connection.query(char_query, function(err, rows) {
+		if(err) {
+			console.log("--error retrieving character list from database");
+			res.status(500).send("Error fetching characters from database: " + err);
+		} 
+		else {
+			var chars = [];
+			console.log(rows);
+
+			rows.forEach(function (row) {
+		        chars.push({
+		          name: row.Character_name
+		        });
+	      	});
+
+	      	res.render('create', {
+				title: 'Create Marathon',
+				chars: chars
+			});	
+
+		}
 	});
+		
 });
 
 app.get('/view', function(req, res) {
@@ -35,7 +58,9 @@ app.get('/view', function(req, res) {
 	});
 });
 
+
 app.use('/public', express.static('public'));
+//app.use(express.static(path.join(__dirname, '/public')));
 
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
@@ -46,6 +71,7 @@ var connection = mysql.createConnection({
 });
 
 var query_test = "SELECT * FROM Episode";
+
 connection.connect(function(err){
   if(err){
     console.log('Error connecting to Db');
@@ -54,14 +80,8 @@ connection.connect(function(err){
   console.log('Connection established');
 });
 
-connection.query(query_test, function(err, rows) {
-	if(err) {
-		console.log(err);
-	}
-	console.log(rows);
-})
-
-
+var char_query = "SELECT Character_name FROM CT";
+var plot_query = "SELECT type FROM Plot_Type";
 
 
 
